d81573de6c7103c55b6622949b0ff213f5dcfe33
Lou Fontana <loufontana@gmail.com>
2015-11-13 17:15:43 -0700

    better API call

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-// url comes from the http://archive.org/advancedsearch.php search
+// url comes from the http://archive.org/advancedsearch.php search. Pulls the following fields from the Grateful Dead collection: coverage, date, description, downloads, title, year & identifier.
+var url = 'http://archive.org/advancedsearch.php?q=collection%3AGratefulDead&fl%5B%5D=coverage&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=identifier&fl%5B%5D=title&fl%5B%5D=year&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=20000&page=1&output=json&callback=callback&save=yes';
 
-var url = 'http://archive.org/advancedsearch.php?q=collection%3AGratefulDead&fl%5B%5D=coverage&fl%5B%5D=date&fl%5B%5D=description&fl%5B%5D=downloads&fl%5B%5D=title&fl%5B%5D=year&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows=50000&page=1&output=json&callback=callback&save=yes';
 // uses lodash _.groupBy function to group the results by date.
 $.ajax({
   url: url, dataType: 'jsonp', success: function (data) {
