3ff075f5ded782cea3fe8418178f2563378399e7
Lou Fontana <loufontana@gmail.com>
2015-11-18 17:50:09 -0700

    Separated search pages

--- datesearch.js
+++ datesearch.js
@@ -51,39 +51,3 @@ $("#dateSearchBtn").click(function() {
           console.log ("No shows on this date in the Archive");
         }
  });
-
-
-
-
-
-
-// Search by City
-
-// $("#citySearchBtn").click(function() {
-//   var citySearch = $('#citySearchText').val()
-//   //console.log(citySearch)
-//   var allConcerts = JSON.parse(localStorage.getItem('data'));
-//   // console.log(allConcerts)
-//   var allConcertsByCity = []
-//   for (var concert in allConcerts) {
-//     if (allConcerts[concert].coverage == citySearch) {
-//       allConcertsByCity.push(allConcerts[concert])
-//     }
-//   }
-//   if (allConcertsByCity.length==0){
-//     console.log ("No shows for this city in the Archive")
-//   }
-//   else {
-//     for (var i = 0; i < allConcertsByCity.length; i++) {
-//       var foundConcert = allConcertsByCity[i]
-//       var $searchResults = $('#searchResults')
-//       var $citySearchResult = $('<h3 data-date = "'+ foundConcert.date+'" style="color: white">' + foundConcert.title +'</h3>')
-//       $citySearchResult.on ('click', function(){
-//         var concerts = allConcerts[this.dataset.date]
-//         console.log(concerts.identifier);
-//       })
-//       $searchResults.append ($citySearchResult)
-//     }
-//   }
-//   console.log(allConcertsByCity)
-// });
